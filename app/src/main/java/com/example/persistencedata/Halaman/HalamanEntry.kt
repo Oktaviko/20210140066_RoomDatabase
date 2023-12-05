690fa87e012921f06aa3dfa491011b18a8116f9a
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2023-12-05 20:36:49 +0700

    add text in button

--- app/src/main/java/com/example/persistencedata/Halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/persistencedata/Halaman/HalamanEntry.kt
@@ -6,9 +6,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
@@ -89,7 +91,7 @@ fun EntrySiswaBody(
                 shape = MaterialTheme.shapes.small,
                 modifier = Modifier.fillMaxWidth()
             ){
-
+                Text(stringResource(id = R.string.btn_submit))
             }
         }
     }
