bf54be41023eada628351b110b5563530a9d8a58
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2023-12-05 20:08:03 +0700

    isi file class Application AplikasiSiswa

--- app/src/main/java/com/example/persistencedata/AplikasiSiswa.kt
+++ app/src/main/java/com/example/persistencedata/AplikasiSiswa.kt
@@ -1,7 +1,14 @@
 package com.example.persistencedata
 
 import android.app.Application
+import com.example.persistencedata.repositori.ContainerApp
+import com.example.persistencedata.repositori.ContainerDataApp
 
 class AplikasiSiswa : Application() {
+    lateinit var container : ContainerApp
 
+    override fun onCreate(){
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
 }
