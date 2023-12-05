d5099e4132e2ba3ec5e6e2a20ea088c9ebba172b
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2023-12-05 22:20:25 +0700

    revisi import

--- app/src/main/java/com/example/persistencedata/Halaman/HalamanHome.kt
+++ app/src/main/java/com/example/persistencedata/Halaman/HalamanHome.kt
@@ -1,6 +1,7 @@
 package com.example.persistencedata.Halaman
 
 import android.provider.ContactsContract.CommonDataKinds.Phone
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Phone
@@ -28,8 +30,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.persistencedata.R
@@ -38,6 +42,7 @@ import com.example.persistencedata.model.HomeViewModel
 import com.example.persistencedata.model.PenyediaViewModel
 import com.example.persistencedata.navigasi.DestinasiNavigasi
 import com.example.persistencedata.navigasi.SiswaTopAppBar
+
 
 object DestinasiHome : DestinasiNavigasi {
     override val route = "home"
