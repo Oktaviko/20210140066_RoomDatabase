71a1768b8f438e344b9d51851ad9f9623c435dd5
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2023-12-05 20:31:46 +0700

    add Column in EntrySiswaBody

--- app/src/main/java/com/example/persistencedata/Halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/persistencedata/Halaman/HalamanEntry.kt
@@ -1,5 +1,7 @@
 package com.example.persistencedata.Halaman
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
@@ -11,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.persistencedata.R
@@ -70,5 +73,10 @@ fun EntrySiswaBody(
     onSiswaValueChange: (DetailSiswa) -> Unit,
     onSaveClick: () -> Unit,
     modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_large)),
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+    ){
 
+    }
 }
