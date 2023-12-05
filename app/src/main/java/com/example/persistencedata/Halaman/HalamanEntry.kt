708f986895edace094d9172378c38e097710cac8
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2023-12-05 20:20:43 +0700

    create function entrysiswascreen

--- app/src/main/java/com/example/persistencedata/Halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/persistencedata/Halaman/HalamanEntry.kt
@@ -1,6 +1,11 @@
 package com.example.persistencedata.Halaman
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.persistencedata.R
+import com.example.persistencedata.model.EntryViewModel
+import com.example.persistencedata.model.PenyediaViewModel
 import com.example.persistencedata.navigasi.DestinasiNavigasi
 
 object DestinasiEntry: DestinasiNavigasi {
@@ -8,3 +13,11 @@ object DestinasiEntry: DestinasiNavigasi {
     override val titleRes = R.string.entry_siswa
 }
 
+@Composable
+fun EntrySiswaScreen(
+    navigateBack: () -> Unit,
+    modifier : Modifier = Modifier,
+    viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
+    ){
+
+}
